bd96a81606c620f19619e9b5ea2cc7a6f29496b4
ambarja <antony.barja8@gmail.com>
2026-03-11 16:23:51 -0500

    script base

--- main.R
+++ main.R
@@ -2,11 +2,35 @@ pacman::p_load(
   tidyverse,
   sf,
   rgee,
-  geoidep
+  geoidep,
+  cptcity
 )
 
-departamentos <- get_departaments()
-distritos <- get_districts()
+ee_Initialize()
+
+# INPUT  ---
+DEPARTAMENTO <- 'LORETO'
 
 
+# Data processing  ---
+dep <- get_departaments() |> 
+  filter(nombdep == DEPARTAMENTO)
 
+dist <- get_districts(departamento = DEPARTAMENTO)
+
+gf_day_1km <- ee$ImageCollection("projects/sat-io/open-datasets/gap-filled-lst/gf_day_1km")$
+  mean() |> 
+  ee$Image$multiply(0.1)
+
+tmean <- ee_extract(
+  x = gf_day_1km,
+  y = dist,
+  fun = ee$Reduce$mean(),
+  sf = TRUE
+)
+
+# Design map
+map <-
+  ggplot() + 
+  geom_sf(data = tmean, aes(fill = )) + 
+  geom_sf(data = dep, aes(fill = NULL), alpha = 0)
