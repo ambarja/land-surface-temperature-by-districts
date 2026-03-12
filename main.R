92b78b5019ad048620d4a18e7886fe9d66bf4f91
Antony Marcos Barja Ingaruca <mvcs_dgppvu_ambi@vivienda.gob.pe>
2026-03-12 09:45:24 -0500

    updated

--- main.R
+++ main.R
@@ -1,36 +1,41 @@
 pacman::p_load(
   tidyverse,
-  sf,
-  rgee,
+  land4health,
   geoidep,
-  cptcity
-)
+  cptcity)
 
 ee_Initialize()
 
-# INPUT  ---
+# INPUT  -----------------------------------------------------------------------
 DEPARTAMENTO <- 'LORETO'
 
-
-# Data processing  ---
+# Data processing  -------------------------------------------------------------
 dep <- get_departaments() |> 
   filter(nombdep == DEPARTAMENTO)
 
 dist <- get_districts(departamento = DEPARTAMENTO)
 
-gf_day_1km <- ee$ImageCollection("projects/sat-io/open-datasets/gap-filled-lst/gf_day_1km")$
-  mean() |> 
+# Mean temperature from 2003 to 2020 
+gf_day_1km <- ee$ImageCollection(
+  "projects/sat-io/open-datasets/gap-filled-lst/gf_day_1km")$
+  max() |> 
   ee$Image$multiply(0.1)
 
 tmean <- ee_extract(
   x = gf_day_1km,
   y = dist,
-  fun = ee$Reduce$mean(),
-  sf = TRUE
-)
+  fun = ee$Reducer$mean(),
+  sf = TRUE,
+  scale = 1000)
 
-# Design map
-map <-
+# Design map -------------------------------------------------------------------
+(map <-
   ggplot() + 
-  geom_sf(data = tmean, aes(fill = )) + 
-  geom_sf(data = dep, aes(fill = NULL), alpha = 0)
+  geom_sf(data = tmean, aes(fill = b1)) +
+  geom_sf(data = dep, aes(fill = NULL), alpha = 0) + 
+  scale_fill_gradientn(colours = cpt(pal = 'ocal_reds',rev = T))
+ )
+
+
+
+
