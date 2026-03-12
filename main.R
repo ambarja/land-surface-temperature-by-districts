f81a28ea759af46c1b68aa092ae327aa65e3c3d5
Antony Marcos Barja Ingaruca <mvcs_dgppvu_ambi@vivienda.gob.pe>
2026-03-12 10:33:04 -0500

    updated

--- main.R
+++ main.R
@@ -2,12 +2,14 @@ pacman::p_load(
   tidyverse,
   land4health,
   geoidep,
-  cptcity)
+  cptcity,
+  extrafont
+  )
 
 ee_Initialize()
 
 # INPUT  -----------------------------------------------------------------------
-DEPARTAMENTO <- 'LORETO'
+DEPARTAMENTO <- 'CUSCO'
 
 # Data processing  -------------------------------------------------------------
 dep <- get_departaments() |> 
@@ -18,24 +20,32 @@ dist <- get_districts(departamento = DEPARTAMENTO)
 # Mean temperature from 2003 to 2020 
 gf_day_1km <- ee$ImageCollection(
   "projects/sat-io/open-datasets/gap-filled-lst/gf_day_1km")$
-  max() |> 
+  mean() |> 
   ee$Image$multiply(0.1)
 
 tmean <- ee_extract(
   x = gf_day_1km,
   y = dist,
-  fun = ee$Reducer$mean(),
+  fun = ee$Reducer$max(),
   sf = TRUE,
   scale = 1000)
 
 # Design map -------------------------------------------------------------------
 (map <-
   ggplot() + 
-  geom_sf(data = tmean, aes(fill = b1)) +
-  geom_sf(data = dep, aes(fill = NULL), alpha = 0) + 
-  scale_fill_gradientn(colours = cpt(pal = 'ocal_reds',rev = T))
+  geom_sf(data = tmean, aes(fill = b1),linewidth = 0.05) +
+  geom_sf(data = dep, aes(fill = NULL), alpha = 0, color = "black") + 
+  scale_fill_gradientn(
+    name = 'Tmean °C',
+    colours = cpt(pal = 'jjg_neo10_base_red_pink'),
+    guide = guide_colorbar(
+     barheight = unit(5, "cm"), 
+     barwidth  = unit(0.4, "cm"))
+    ) + 
+   theme_void(base_family = 'Josefin Slab',base_size = 14) 
  )
 
-
-
-
+ggsave(
+  filename = paste0(str_to_lower(DEPARTAMENTO),'.svg'),
+  plot = last_plot()
+  )
